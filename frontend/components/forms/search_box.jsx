3e4b07b03ec096a579d9e9fbc401801186fe4465
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-12 23:26:38 -0500

    Created skeleton for booking (search box) dropdown for # of guests

--- frontend/components/forms/search_box.jsx
+++ frontend/components/forms/search_box.jsx
@@ -3,9 +3,63 @@ import React from 'react';
 class SearchBox extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            dropdownOpen: true,
+            kidsCount: 0,
+            petsCount: 0,
+            parentsCount: 0
+        }
+
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+
+    toggleDropdown() {
+        this.setState({ dropdownOpen: !this.state.dropdownOpen});
+    }
+
+    openDropdown() {
+        this.setState({ dropdownOpen: true });
+    }
+    
+    closeDropdown() {
+        this.setState({ dropdownOpen: false });
     }
 
     render() {
+
+        const dropdownMenu = (
+            <div className="search-box-dropdown-container">
+                <ul>
+                    <li>
+                        <span>Kids</span>
+                        <div className="search-box-counter-container">
+                            <div className="search-box-minus-circle">-</div>
+                            <span>{this.state.kidsCount}+</span>
+                            <div className="search-box-plus-circle">+</div>
+                        </div>
+                    </li>
+                    <li>
+                        <span>Pets</span>
+                        <div className="search-box-counter-container">
+                            <div className="search-box-minus-circle">-</div>
+                            <span>{this.state.petsCount}+</span>
+                            <div className="search-box-plus-circle">+</div>
+                        </div>
+                    </li>
+                    <li>
+                        <span>Parents</span>
+                        <div className="search-box-counter-container">
+                            <div className="search-box-minus-circle">-</div>
+                            <span>{this.state.parentsCount}+</span>
+                            <div className="search-box-plus-circle">+</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        )
+
+        let dropdownComponent = this.state.dropdownOpen ? dropdownMenu : <></>;
+
         return (
             <div className="splash-image-container">
                 <div className="search-box-container">
@@ -47,7 +101,14 @@ class SearchBox extends React.Component {
                             <input
                                 className="search-box-input"
                                 type="text"
-                                placeholder="Guests" />
+                                placeholder="Guests" 
+                                onMouseDown={() => this.toggleDropdown()}
+                                onBlur={() => this.closeDropdown()}/>
+                            <div
+                                onFocus={() => this.openDropdown()}
+                                onBlur={() => this.closeDropdown()}
+                                tabIndex="0"
+                            >{dropdownComponent}</div>
                         </label>
                         <input
                             className="search-box-submit-btn"
